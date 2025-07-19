7f8ee14aa12bb7e74bccaaf0a15dc825a88b5a5a
Aditya Kumar <Workwithaadi@gmail.com>
2025-07-19 20:04:32 +0530

    inhancements done - jwt,bcrypt

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 const path = require("path");
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 
 const PORT = 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // for parsing application/json
 app.use(express.static("public"));
+app.use(cookieParser());
 
 //Azure Mongo url
 //const MONGO_URL = "mongodb://bitsuserregappproject:64gEFIwn1tbnbxXviKqDm9TMCj2G0fBGWMjfrOUwGeTvi257gSCAezqkc6m6mrTn7JzLZb50R5GNACDb993aug==@bitsuserregappproject.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@bitsuserregappproject@";
@@ -23,24 +26,25 @@ db.once('open', () => {
 
 //Create a user schema and model
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true },
-    password: String,
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true }
 });
-
 const User = mongoose.model('User', userSchema);
 
 
 // GET all users
-app.get("/getUsers", async (req, res) => {
+app.get("/getUsers", verifyToken, async (req, res) => {
+    //res.status(200).json({ message: 'Protected route accessed' });
     try {
-        await client.connect();
-        const db = client.db("userdb");
-        const data = await db.collection("users").find({}).toArray();
-        res.send(data);
+        const users = await User.findOne({});
+        const currentUsername = res.status(200).json(users.username);
+        return currentUsername;
+        //await client.connect();
+        // const db = client.db("userdb");
+        // const data = await db.collection("users").find({}).toArray();
+        // res.send(data);
     } catch (err) {
         res.status(500).send("Error fetching users");
-    } finally {
-        await client.close();
     }
 });
 
@@ -51,10 +55,8 @@ app.post("/addUser", async (req, res) => {
     try {
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
-
         const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
-
         res.status(201).send('User registered successfully');
     } catch (error) {
         console.error(error);
@@ -62,29 +64,60 @@ app.post("/addUser", async (req, res) => {
     }
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.set('view engine', 'ejs');
+app.get('/dashboard', verifyToken, async (req, res) => {
+    const users = await User.findOne({});
+    res.sendFile(path.join(__dirname, 'views', 'dashboard.ejs'));
+    res.render('dashboard', { user: users.username });
+});
+
 // POST login API
-app.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-
+app.post('/login', async (req, res) => {
     try {
+        const { username, password } = req.body;
         const user = await User.findOne({ username });
-
         if (!user) {
-            return res.status(404).send('User not found');
+            return res.status(401).json({ error: 'Authentication failed' });
         }
-
         const passwordMatch = await bcrypt.compare(password, user.password);
-
-        if (passwordMatch) {
-            res.send('Login successful');
-        } else {
-            res.status(401).send('Invalid credentials');
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Authentication failed' });
         }
+        const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
+            expiresIn: '48h',
+        });
+        res.cookie("token", token);
+        res.redirect('/dashboard');
+        //res.status(200).json({ token });
     } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred');
+        res.status(500).json({ error: 'Login failed' });
     }
 });
+
+function verifyToken(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) return res.redirect('/login');
+
+    try {
+        const decoded = jwt.verify(token, 'your-secret-key');
+        if (decoded && decoded.userId) {
+            req.userId = decoded.userId;
+        } else {
+            return res.redirect('/login');
+        }
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+};
+
+// app.get('/', verifyToken, (req, res) => {
+//     res.status(200).json({ message: 'Protected route accessed' });
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
